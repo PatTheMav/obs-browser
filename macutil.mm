d1cf6bb02933be8b0291c7086e389e8d5bf7ed49
Colin Edwards <colin@recursivepenguin.com>
2021-01-07 18:52:11 -0600

    Disable hwaccel on 10.14 and lower

---
+++ macutil.mm
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+bool atLeast10_15(void) {
+        NSProcessInfo *processInfo = [[NSProcessInfo alloc] init];
+	bool atLeast = [processInfo isOperatingSystemAtLeastVersion: (NSOperatingSystemVersion){10,15,0}];
+	[processInfo release];
+
+	return atLeast;
+}
