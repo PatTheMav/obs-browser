e81d5038c68a35a347a24ad7d3152ca0e74f29db
PatTheMav <PatTheMav@users.noreply.github.com>
2023-12-20 19:24:11 +0100

    cmake: Remove runtime library override on Windows
    The CEF wrapper library needs to be compiled with the correct
    runtime library selected on Windows per CEF's documentation.
    
    Removing the overrides ensures that the project is only ever linked
    with an appropriately generated wrapper library.

--- cmake/os-windows.cmake
+++ cmake/os-windows.cmake
@@ -1,8 +1,6 @@
-target_compile_options(obs-browser PRIVATE $<IF:$<CONFIG:DEBUG>,/MTd,/MT>)
 target_compile_definitions(obs-browser PRIVATE ENABLE_BROWSER_SHARED_TEXTURE)
 
 target_link_libraries(obs-browser PRIVATE CEF::Wrapper CEF::Library d3d11 dxgi)
-target_link_options(obs-browser PRIVATE /IGNORE:4099)
 
 add_executable(obs-browser-helper WIN32 EXCLUDE_FROM_ALL)
 add_executable(OBS::browser-helper ALIAS obs-browser-helper)
@@ -16,11 +14,10 @@ target_sources(
 target_include_directories(obs-browser-helper PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/deps"
                                                       "${CMAKE_CURRENT_SOURCE_DIR}/obs-browser-page")
 
-target_compile_options(obs-browser-helper PRIVATE $<IF:$<CONFIG:DEBUG>,/MTd,/MT>)
 target_compile_definitions(obs-browser-helper PRIVATE ENABLE_BROWSER_SHARED_TEXTURE)
 
 target_link_libraries(obs-browser-helper PRIVATE CEF::Wrapper CEF::Library nlohmann_json::nlohmann_json)
-target_link_options(obs-browser-helper PRIVATE /IGNORE:4099 /SUBSYSTEM:WINDOWS)
+target_link_options(obs-browser-helper PRIVATE /IGNORE:4098 /SUBSYSTEM:WINDOWS)
 
 set(OBS_EXECUTABLE_DESTINATION "${OBS_PLUGIN_DESTINATION}")
 set_target_properties_obs(
